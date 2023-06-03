104f39d6bea8835b72bbe4efa81a0e5c7f24ccde
hossein <hosseinfalah2021@gmail.com>
2023-06-03 20:11:03 +0330

    complete Home Page

--- js/index.js
+++ js/index.js
@@ -7,3 +7,54 @@ const swiper = new Swiper(".mySwiper", {
     grabCursor: true,
 
 });
+
+const LastedProductSwiper = new Swiper(".lastedProduct", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    autoplay: {
+        delay: 3000,
+    },
+    grabCursor: true,
+    breakpoints: {
+        // when window width is >= 480px
+        480: {
+            slidesPerView: 1,
+            spaceBetween: 30
+        },
+        // when window width is >= 640px
+        640: {
+            slidesPerView: 2,
+            spaceBetween: 40
+        },
+        1200: {
+            slidesPerView: 3,
+            spaceBetween: 40
+        }
+    }
+
+});
+
+const LastedArticlesSwiper = new Swiper(".lastedArticles", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    autoplay: {
+        delay: 3000,
+    },
+    grabCursor: true,
+    breakpoints: {
+        // when window width is >= 480px
+        480: {
+            slidesPerView: 1,
+            spaceBetween: 30
+        },
+        // when window width is >= 640px
+        640: {
+            slidesPerView: 2,
+            spaceBetween: 40
+        },
+        1200: {
+            slidesPerView: 3,
+            spaceBetween: 40
+        }
+    }
+});
