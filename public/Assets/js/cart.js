b2524b8f8e3d3b72079414b34213da5c734359c1
hossein <hosseinfalah2021@gmail.com>
2023-06-04 20:56:07 +0330

    added shopping cart page

---
+++ public/Assets/js/cart.js
@@ -0,0 +1,23 @@
+const $ = document;
+
+const allTabs = $.querySelectorAll('#btn_links');
+const allContent = $.querySelectorAll('.tab_content');
+
+allTabs.forEach(tab => {
+    tab.addEventListener('click', (e) => TabChange(e))
+})
+
+const TabChange = (e) => {
+    //Find Id Content Your Clicked
+    const mainContentID = e.target.dataset.id;
+    //Find Content Your Clicked
+    const mainContent = $.querySelector(`#${mainContentID}`);
+    // Hidden All Content
+    allContent.forEach(content => content.classList.remove('active'));
+    //Show Content
+    mainContent.classList.add('active');
+    console.log(mainContent);
+}
+
+console.log(allTabs);
+console.log(allContent);
