73f22375e896cbc5cc22a8fed5504e72f04728d7
hossein <hosseinfalah2021@gmail.com>
2023-06-05 18:39:15 +0330

    fix responsve

--- public/Assets/js/article.js
+++ public/Assets/js/article.js
@@ -6,3 +6,28 @@ const swiper = new Swiper(".article-swiper", {
     },
     grabCursor: true,
 });
+
+const lastedArticlesSwiper = new Swiper(".lastedArticles", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    autoplay: {
+        delay: 3000,
+    },
+    grabCursor: true,
+    breakpoints: {
+        // when window width is >= 480px
+        480: {
+            slidesPerView: 1,
+            spaceBetween: 30
+        },
+        // when window width is >= 640px
+        640: {
+            slidesPerView: 2,
+            spaceBetween: 40
+        },
+        1200: {
+            slidesPerView: 3,
+            spaceBetween: 40
+        }
+    }
+});
---
+++ public/Assets/js/articles.js
@@ -0,0 +1,8 @@
+const swiper = new Swiper(".article-swiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    autoplay: {
+        delay: 3000,
+    },
+    grabCursor: true,
+});
