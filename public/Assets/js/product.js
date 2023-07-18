09e50742a0f2d25c53be319b05d623d0832de5b2
HosseinFalah <98707258+HosseinFalah@users.noreply.github.com>
2023-07-18 23:36:25 +0330

    create function copylink page use btn

--- public/Assets/js/product.js
+++ public/Assets/js/product.js
@@ -13,10 +13,30 @@ const $ = document
 const btnWrapper = $.querySelector('.btn_wrapper')
 const allTabs = $.querySelectorAll('#tab_links');
 const allContents = $.querySelectorAll('.tab_content');
-
+const btnCopyLink = $.querySelector('#copy-link');
 const tabImg = $.querySelectorAll('#product_details');
 const mainImg = $.querySelector('#mainProductImg');
 const mainImgModal = $.querySelector('#modal_product_img');
+
+const copyLinkClipBord = () => {
+    const Link = location.href;
+    navigator.clipboard.writeText(Link)
+        .then(() => {
+            swal({
+                title: "عملیات موفقعیت آمیزبود",
+                text: "لینک صفحه با موفقعیت کپی شد",
+                icon: "success",
+                button: "اوکی",
+              });
+        })
+}
+
+tabImg.forEach(tab => {
+    tab.addEventListener('click', (e) => {
+        mainImg.src = e.target.src;
+        mainImgModal.src = e.target.src;
+    });
+});
 
 btnWrapper.addEventListener('click', (e) => {
     const mainContentID = e.target.dataset.id;
@@ -26,9 +46,4 @@ btnWrapper.addEventListener('click', (e) => {
     mainContent.classList.add('active');
 });
 
-tabImg.forEach(tab => {
-    tab.addEventListener('click', (e) => {
-        mainImg.src = e.target.src;
-        mainImgModal.src = e.target.src;
-    });
-});
+btnCopyLink.addEventListener('click', () => copyLinkClipBord())
